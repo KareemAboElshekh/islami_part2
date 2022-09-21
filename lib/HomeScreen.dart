d23b21534d1457dd73cd951524d038cd712bb97f
KAREEM <sakr9682@gmail.com>
2022-09-21 16:26:36 +0200

    Initial commit

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String ruteName = "Home";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Screen"),
+      ),
+    );
+  }
+}
